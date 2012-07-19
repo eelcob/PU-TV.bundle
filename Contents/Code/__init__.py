d5aafe2ef4331a235d8806a974382ac5c7ac0096
sander1 <sander@plexapp.com>
2012-07-19 18:07:31 +0200

    Put some code together on 1 line; strip whitespace from summaries; return oc instead if messagecontainer if 0 videos are found.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,6 +5,7 @@ NAME 			= L("Title")
 
 BASEURL = "http://www.pu.nl"
 PUTVURL = BASEURL + "/pu-tv"
+
 ####################################################################################################
 def Start():
 
@@ -15,41 +16,39 @@ def Start():
 	ObjectContainer.title1 = NAME
 	ObjectContainer.view_group = "List"
 	ObjectContainer.art = R(ART)
-	
+
 	VideoClipObject.thumb = R(ICON)
 
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:13.0) Gecko/20100101 Firefox/13.0.1"
 
-
 ####################################################################################################
 def MainMenu(page=0):
+
 	oc = ObjectContainer()
-	
-	pagestr=str(page)
-	url = PUTVURL + "?page=" + pagestr
+	url = PUTVURL + "?page=" + str(page)
 
 	for clip in HTML.ElementFromURL(url).xpath("//div[contains(@class ,'PU-TV')]"):
 		clip_link 	= BASEURL + clip.xpath(".//div[@class='field_image']/a")[0].get("href")
 		clip_thumb 	= clip.xpath(".//div[@class='field_image']/a/img")[0].get("src")
 		clip_title	= clip.xpath(".//div[@class='views-field views-field-title']/span/a")[0].text
 		try:
-			clip_desc	= clip.xpath("./text()[normalize-space()]")[0]
+			clip_desc	= clip.xpath("./text()[normalize-space()]")[0].strip()
 		except:
 			clip_desc	= ""
-	
+
 		oc.add(VideoClipObject(
 			url = clip_link,
 			title = clip_title,
 			summary = clip_desc,
-			thumb=Resource.ContentsOfURLWithFallback(url=clip_thumb, fallback=ICON)
+			thumb = Resource.ContentsOfURLWithFallback(url=clip_thumb, fallback=ICON)
 		))
 
 	if len(oc) == 0:
-		return MessageContainer(L('NoVideo'))
-		
+		return ObjectContainer(header = L('NoVideo'), message = L('NoVideo'))
+
 	else:
 		if len(clip.xpath('//li[@class="pager-next"]')) > 0:
 			oc.add(DirectoryObject(key=Callback(MainMenu, page=page+1), title=L('More'), thumb=R(ICON_MORE)))
-	
+
 	return oc
